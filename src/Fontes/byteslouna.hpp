1fe9531fe00c1cdf216e4039c9fb54d7339c7cd5
Fluffy <fluffy@example.com>
2026-03-21 11:48:48 -0400

    Distribution: Binary-only version (EXE and DLLs)

--- src/Fontes/byteslouna.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-extern unsigned char lounafont[];
-extern unsigned int sizefontlouna;
--- src/Includes/ImGui/origens.h
+++
@@ -1,5 +0,0 @@
-// fonts.h
-#pragma once
-#include "Includes/ImGui/Files/imgui.h"
-
-extern ImFont* lexend_font;
